d1873e7da9af36e705e754d1a0a3de29b991adda
Grey Warden <gandalfdude@gmail.com>
2023-01-30 20:24:19 +0300

    removed hard-coded persistent unit name

--- src/main/java/dao/util/ConnectionManager.java
+++ src/main/java/dao/util/ConnectionManager.java
@@ -10,9 +10,10 @@ import javax.persistence.Persistence;
 public class ConnectionManager implements IConnection {
 
     private final EntityManagerFactory factory;
+    public static final String PERSISTENT_UNIT_NAME = "voting";
 
     public ConnectionManager() {
-        factory = Persistence.createEntityManagerFactory("voting");
+        factory = Persistence.createEntityManagerFactory(PERSISTENT_UNIT_NAME);
     }
 
     @Override
