50e3c342947364ac02cc024132524c09adcb5c37
Alexander <gandalfdude@gmail.com>
2023-02-02 19:06:02 +0300

    cleaned up code

--- src/main/java/dao/database/VoteDBDAO.java
+++ src/main/java/dao/database/VoteDBDAO.java
@@ -10,7 +10,6 @@ import dto.SavedVoteDTO;
 import dto.VoteDTO;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -20,42 +19,6 @@ import java.util.stream.Collectors;
 public class VoteDBDAO implements IVoteDAO {
     @Override
     public List<SavedVoteDTO> getAll() {
-        List<SavedVoteDTO> savedVoteDTOs;
-        List<VoteEntity> voteEntities;
-        EntityManager entityManager = null;
-        try {
-            entityManager = ConnectionSingleton.getInstance().open();
-            entityManager.getTransaction().begin();
-            entityManager.createNativeQuery("SET TRANSACTION READ ONLY;").executeUpdate();
-
-            Query query = entityManager.createQuery("SELECT v FROM VoteEntity v");
-            voteEntities = query.getResultList();
-
-            savedVoteDTOs = voteEntities
-                    .stream()
-                    .map(vote -> new SavedVoteDTO(new VoteDTO(
-                            vote.getArtistId().getId(),
-                            vote.getGenreIds().stream().map(GenreEntity::getId).collect(Collectors.toList()),
-                            vote.getAbout(),
-                            vote.getEmail()
-                    ), vote.getCreationTime()))
-                    .collect(Collectors.toList());
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            if (entityManager != null && entityManager.getTransaction().isActive()) {
-                entityManager.getTransaction().rollback();
-            }
-            throw e;
-        } finally {
-            if (entityManager != null) {
-                entityManager.close();
-            }
-        }
-
-        return savedVoteDTOs;
-    }
-
-    public List<SavedVoteDTO> getAllCriteria() {
         List<SavedVoteDTO> savedVoteDTOs;
         List<VoteEntity> voteEntities;
         EntityManager entityManager = null;
@@ -100,38 +63,6 @@ public class VoteDBDAO implements IVoteDAO {
     }
     @Override
     public void save(SavedVoteDTO vote) {
-        EntityManager entityManager = null;
-        try {
-            entityManager = ConnectionSingleton.getInstance().open();
-
-            entityManager.getTransaction().begin();
-
-            ArtistEntity artistEntity = entityManager.find(ArtistEntity.class,
-                    vote.getVoteDTO().getArtistId());
-
-            List<Long> genreIds = vote.getVoteDTO().getGenreIds();
-            Query getGenres = entityManager.createQuery("SELECT g FROM GenreEntity g WHERE id IN (:genreIds)");
-            getGenres.setParameter("genreIds", genreIds);
-            List<GenreEntity> genres = getGenres.getResultList();
-
-            VoteEntity voteEntity = new VoteEntity(artistEntity, vote.getVoteDTO().getAbout(),
-                    genres, vote.getCreateDataTime(), vote.getVoteDTO().getEmail());
-            entityManager.persist(voteEntity);
-
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            if (entityManager != null && entityManager.getTransaction().isActive()) {
-                entityManager.getTransaction().rollback();
-            }
-            throw e;
-        } finally {
-            if (entityManager != null) {
-                entityManager.close();
-            }
-        }
-    }
-
-    public void saveCriteria(SavedVoteDTO vote) {
 
         EntityManager entityManager = null;
         List<GenreEntity> genres;
